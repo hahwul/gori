70347b6e01b84e92022470b33bd7c2d070d88c92
HAHWUL <hahwul@gmail.com>
2026-07-04 10:11:01 +0900

    remove test_encoding.cr
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- test_encoding.cr
+++
@@ -1,6 +0,0 @@
-# Test String.new() behavior with invalid UTF-8
-invalid_bytes = Bytes[0xFF, 0xFE, 0x41] # Invalid UTF-8 sequence + valid 'A'
-str = String.new(invalid_bytes)
-puts "Created string: #{str.inspect}"
-puts "Valid encoding? #{str.valid_encoding?}"
-puts "Bytes: #{str.bytes.inspect}"
