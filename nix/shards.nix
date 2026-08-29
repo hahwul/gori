e34ccabd247f6129720bd77177eb5b5b72596d2c
hahwul <hahwul@gmail.com>
2026-08-29 22:47:55 +0900

    update shards.nix
    Signed-off-by: hahwul <hahwul@gmail.com>

--- nix/shards.nix
+++ nix/shards.nix
@@ -1,13 +1,23 @@
 {
   "ameba" = {
     url = "https://github.com/crystal-ameba/ameba.git";
-    rev = "cdd58b34b0d8a9c785d67183a7a8f07541549e55";
-    sha256 = "1bkdjhimbipqn6lqw834q1wb97k9qf75sk1brnigynbjzy43q44m";
+    rev = "v1.7.0";
+    sha256 = "0blfyizcajyrgbfxhiwnvf1xmz3wfx6kpf9jf8x7daxihany4r9z";
+  };
+  "cvss" = {
+    url = "https://github.com/hahwul/cvss.cr.git";
+    rev = "v0.2.0";
+    sha256 = "16fm9c7l0g3jgk9r8kjcaw5c197w36d0irl6iixmqpaavnqjsgvk";
   };
   "db" = {
     url = "https://github.com/crystal-lang/crystal-db.git";
     rev = "v0.14.0";
     sha256 = "1s67fs5abzgg2yyjsm2la967mk881i91h8jdnal072fb9qh9wr58";
+  };
+  "sarif" = {
+    url = "https://github.com/hahwul/sarif.cr.git";
+    rev = "v0.2.0";
+    sha256 = "0rd3yw20djr4sc3lnimj9hiv9aqn6vxf3cz48wpkgr7dmwqlfn6g";
   };
   "sqlite3" = {
     url = "https://github.com/crystal-lang/crystal-sqlite3.git";
